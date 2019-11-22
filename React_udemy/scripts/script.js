31d61952afcb858bc34196309e4b01dd614609f1
Thomas Rio <a.thomas.rio@gmail.com>
2019-11-21 20:41:04 -0500

    after adding npm

--- React_udemy/scripts/script.js
+++ React_udemy/scripts/script.js
@@ -1,31 +1,39 @@
+// import { without } from 'lodash';
+
+var _ = require('lodash');
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log('answer:', _.without(array, 3));
+
+
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
 var button = document.querySelector('button');
 
 function setColorsAndText() {
-	var css = document.querySelector('h3');
+    var css = document.querySelector('h3');
 
-	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
-	css.textContent = body.style.background + ';';
+    body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+    css.textContent = body.style.background + ';';
 }
 
 function getRandomColor() {
-	var letters = '0123456789ABCDEF'.split('');
-	var color = '#';
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
 
-	for (var i = 0; i < 6; i++) {
-		color += letters[Math.round(Math.random() * 16)];
-	}
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.round(Math.random() * 16)];
+    }
 
-	return color;
+    return color;
 }
 
 function setRandomColors() {
-	color1.value = getRandomColor();
-	color2.value = getRandomColor();
-	
-	setColorsAndText();
+    color1.value = getRandomColor();
+    color2.value = getRandomColor();
+
+    setColorsAndText();
 }
 
 body.addEventListener('onload', setColorsAndText());
